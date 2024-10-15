c9ae7eba6350b404298096bc60e546edd8801c42
Alexis Varsava <alexis.varsava@oicr.on.ca>
2024-10-15 09:51:23 -0400

    Document the tree settings

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ def main():
     args = parser.parse_args()
 
     try:
+        # From manpage:
+        # -a     All files are printed.  By default tree does not print hidden files (those beginning with a dot `.').
+        #   In no event does tree print the file system constructs `.' (current directory) and  `..' (previous directory)
+        # -f     Prints the full path prefix for each file.
+        # --noreport Omits printing of the file and directory report at the end of the tree listing.
+        # -Q     Quote the names of files in double quotes.
+        # -D     Print the date of the last modification time or if -c is used, the last status change time for the
+        #   file listed.
+
         result = subprocess.check_output("tree -a -f --noreport -Q -D {0}".format(args.dir), shell=True, executable="/bin/bash")
 
     except CalledProcessError as cpe:
